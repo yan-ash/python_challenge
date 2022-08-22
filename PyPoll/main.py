6b633e4c12d7c81f7eeedaf1998416fa766db68d
yan-ash <109451707+yan-ash@users.noreply.github.com>
2022-08-22 23:59:07 +1000

    revise2

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,12 +3,12 @@ import csv
 
 
 os.chdir(os.path.dirname(__file__))
-# Optional: print the current working directory
-print("This program is running from: " + os.getcwd())
+
 #-----------------------------------------------------
 
 # Path to collect data from the Resources folder
 csvpath = os.path.join('Resources', 'election_data.csv')
+outputpath=os.path.join('Analysis', 'election_data.txt')
 
 #initialize a total vote counter.
 total_votes=0
@@ -22,24 +22,36 @@ with open (csvpath) as csvfile:
         header= next(csvreader)
        
         
-        for row in csvfile:
+        for row in csvreader:
             total_votes= total_votes+1
-            candidate =(row[2])
+            candidate =str(row[2]) 
             # if the candidate name is not in the candidate list, add candidate in the list
             if candidate not in candidate_list:
               
-                candidate_list= candidate_list. append(candidate)
+                candidate_list. append(candidate)
                 
                 candidates_vote[candidate]=0
 
-            candidates_vote[candidate] += 1
+                candidates_vote[candidate] += 1
+print(total_votes)                    
+print(candidate_list) 
+print(candidates_vote) 
+ 
 
-            
+for candidate in candidates_vote:
+        vote=(candidates_vote).get(candidate)
+        candidate_percentage= float(vote)/float(total_votes) *100
+        candidate_result=(f"{candidate}:{candidate_percentage:.3f}%\n")
+        print(candidate_result)
+with open(outputpath, 'w') as csvtxt:
+            Election_results= print("Election Results")
+            total_votes=print(f'Total Votes: {total_votes}')
+
+       
 
  
-vote_count_sum=sum(vote_count_list)
 
 
-print(candidate_list)
-print(vote_count_sum)
+
+
 
